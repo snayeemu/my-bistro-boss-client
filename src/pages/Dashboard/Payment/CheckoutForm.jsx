7529239d23a010f0acfeca2208efd04219010f19
snayeemu <snayeemu2@gmail.com>
2023-06-10 19:35:14 +0600

    payment error added

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,8 +1,10 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [cardError, setCardError] = useState("");
 
   const handleSubmit = async (event) => {
     // Block native form submission.
@@ -29,13 +31,15 @@ const CheckoutForm = () => {
 
     if (error) {
       console.log("[error]", error);
+      setCardError(error.message);
     } else {
+      setCardError("");
       console.log("[PaymentMethod]", paymentMethod);
     }
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form className="w-2/3 m-8" onSubmit={handleSubmit}>
         <CardElement
           options={{
             style: {
@@ -52,10 +56,15 @@ const CheckoutForm = () => {
             },
           }}
         />
-        <button type="submit" disabled={!stripe}>
+        <button
+          className="btn btn-primary btn-sm mt-4"
+          type="submit"
+          disabled={!stripe}
+        >
           Pay
         </button>
       </form>
+      {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
     </>
   );
 };
